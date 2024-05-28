11d303f764c7f662d349cf52970b8d8655519718
Ahmed <bhaia9036@gmail.com>
2024-05-28 23:06:59 +0500

    end

--- src/App.js
+++ src/App.js
@@ -1,96 +1,79 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import { AuthProvider } from './Components/AuthContext';
-import Student_campaign from './Pages/Student_campaign';
-import Donor from './Pages/Donor';
-import Contact from './Pages/Contact';
-import CampaignDetails from './Pages/CampaignDetails';
-import PaywithSkrill from './Components/PaywithSkrill';
-
+import Home from "./Pages/Home";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import { AuthProvider } from "./Components/AuthContext";
+import Student_campaign from "./Pages/Student_campaign";
+import Donor from "./Pages/Donor";
+import Contact from "./Pages/Contact";
+import CampaignDetails from "./Pages/CampaignDetails";
+import PaywithSkrill from "./Components/PaywithSkrill";
 
 const App = () => {
   return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path={"/"}
+              element={
+                <>
+                  <Login />
+                </>
+              }
+            />
 
-<>
-<AuthProvider>
+            <Route
+              path={"/Signup"}
+              element={
+                <>
+                  <Signup />
+                </>
+              }
+            />
 
-<BrowserRouter>
+            <Route
+              path={"/home"}
+              element={
+                <>
+                  <Home />
+                </>
+              }
+            />
+            <Route
+              path={"/Student_campaign"}
+              element={
+                <>
+                  <Student_campaign />
+                </>
+              }
+            />
 
-<Routes>
+            <Route
+              path={"/Donor"}
+              element={
+                <>
+                  <Donor />
+                </>
+              }
+            />
+            <Route
+              path={"/Contact"}
+              element={
+                <>
+                  <Contact />
+                </>
+              }
+            />
+            <Route path="/campaign/:id" element={<CampaignDetails />} />
+            <Route path="/PaywithSkrill" element={<PaywithSkrill />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  );
+};
 
-<Route
-      path={"/"}
-      element={
-        <>
-     <Login  />
-        </>
-      }
-    />
-
-<Route
-      path={"/Signup"}
-      element={
-        <>
-     <Signup  />
-        </>
-      }
-    />
-
-<Route
-      path={"/home"}
-      element={
-        <>
-     <Home  />
-        </>
-      }
-    />
-<Route
-      path={"/Student_campaign"}
-      element={
-        <>
-     <Student_campaign  />
-        </>
-      }
-    />
-
-<Route
-      path={"/Donor"}
-      element={
-        <>
-     <Donor/>
-        </>
-      }
-    />
-    <Route
-      path={"/Contact"}
-      element={
-        <>
-     <Contact/>
-        </>
-      }
-    />
-        <Route path="/campaign/:id" element={<CampaignDetails />} />
-        <Route path="/PaywithSkrill" element={<PaywithSkrill />} />
-
-
-        
-    </Routes>
-
-
-    
-
-</BrowserRouter>
-
-</AuthProvider>
-
-
-
-</>
-
-    )
-}
-
-export default App
+export default App;
--- src/Pages/Donor.jsx
+++ src/Pages/Donor.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import MegaMenu from '../Components/Navbar'
-import CampaignForm from '../Components/Campaign'
-import Footer from '../Components/Footer'
-import CampaignList from '../Components/CampainList'
+import React from "react";
+import MegaMenu from "../Components/Navbar";
+import CampaignForm from "../Components/Campaign";
+import Footer from "../Components/Footer";
+import CampaignList from "../Components/CampainList";
 
 const Donor = () => {
   return (
     <>
-    <MegaMenu/>
-<div  className='py-24' >
-<h1 className="text-2xl font-bold text-center mb-6   ">Student Campaigns</h1>
-
-
-<CampaignList/>
-</div>
-
-<Footer/>
-    
+      <MegaMenu />
+      <div className="py-24">
+        <h1 className="text-2xl font-bold text-center mb-6   ">
+          Student Campaigns
+        </h1>
+        <CampaignList />
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Donor
+export default Donor;
